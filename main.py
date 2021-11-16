8a919944fd7ce5b06e3e7fdfb3b9e2f6026233f0
clement-05 <clementgarrigouavion@gmail.com>
2021-11-16 23:03:31 +0100

    ajout installation automatique de pygame

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import pygame
+print('Démarrage...')
+
+try : 
+    import pygame
+except ImportError :
+    print("Erreur le module pygame est requis pour le bon fonctionnement de ce jeu")
+    print("Voulez-vous que nous installions automatiquement ce module (Y/n)", end = " ")
+    if input("") != "n" :
+        print("Installation de pygame en cours...")
+        import os
+        command = ("python" if os.name == "nt" else "python3") + " -m pip install pygame"
+        if not os.system(command) :
+            print("Succès ! pygame a bien été installé")
+            import pygame
+        else : 
+            print("Une erreur est survenue veuillez réessayer")
+            exit()
+    else : exit()
+
 pygame.init()
 screen = pygame.display.set_mode([600, 600])
 running = True
