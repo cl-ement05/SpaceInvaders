6d5c918b2e86a48686fc0178f923cd17f980fdcd
Paul Cornea <pcornea@numericable.fr>
2021-12-08 13:14:58 +0100

    ajout images joueur/pioupiou + insertion des déplacements dans joueur

--- joueur.py
+++ joueur.py
@@ -1,16 +1,29 @@
 import pygame
-from pygame.locals import RLEACCEL
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+)
 
 class Joueur(pygame.sprite.Sprite) : 
     def __init__(self) -> None:
-        super(Joueur, self).__init__()                     #permet d'initialiser la classe parente. Equivalent à super().__init__(self)
+        super(Joueur, self).__init__()#permet d'initialiser la classe parente. Equivalent à super().__init__(self)
         self._vie = 3
-        self.surf = pygame.image.load("IMAGEJOUEUR.jpg").convert()
+        self.surf = pygame.image.load("joueur.jpg").convert()
         self.surf.set_colorkey((0, 0, 0), RLEACCEL)
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(center=(350,600))
 
     def isAlive(self) :
         return self._vie > 0
 
-    def update(self, pressed_keys) :
-        pass
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
--- personnage.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Peronnage(pygame.sprite.Sprite) :
-    def __init__(self) -> None:
-        pass
-
-    def isAlive(self) :
-        return self._vie > 0
