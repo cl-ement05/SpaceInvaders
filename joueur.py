2f5a52917f6d9811e8ad428bd8103173795a1380
Paul Cornea <pcornea@numericable.fr>
2021-12-10 17:50:57 +0100

    pas sortir écran merde

--- joueur.py
+++ joueur.py
@@ -27,3 +27,13 @@ class Joueur(pygame.sprite.Sprite) :
             self.rect.move_ip(-5, 0)
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(5, 0)
+
+        #ne pas sortir de la map
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
