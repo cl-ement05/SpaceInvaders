4fc9196367d3a8187c4701ec59da2a9d718f40d3
Paul Cornea <pcornea@numericable.fr>
2021-12-14 17:43:34 +0100

    retrait d'un commentaire qui n'avait plus lieu d'être ( LOST TO PURGE Pepega )

--- pioupiou.py
+++ pioupiou.py
@@ -3,7 +3,6 @@ from pygame.locals import RLEACCEL
 
 class Pioupiou(pygame.sprite.Sprite):
     #définition des attributs : le paramètre position sera la position du joueur en couple de coordonnées
-    #origin permet de savoir si le missile a été créé par le joueur ou par un ennemi
     #direction "up" ou "down" permet de savoir vers ou se dirige le missile
     def __init__(self, position):
         super().__init__()      #permet d'initialiser la classe parente. Equivalent à super().__init__(self)
